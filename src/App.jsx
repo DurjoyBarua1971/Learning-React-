c2cfd6c9c4771f47659a001a69b26342f9fe5f49
Durjoy Barua <durjoy.barua@brainstation-23.com>
2025-04-09 17:23:01 +0600

    Enhance App component styling with Tailwind CSS classes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="font-roboto">
+    <div className="font-roboto container mx-auto flex flex-col items-center justify-center h-screen bg-amber-300 text-gray-800">
       <div>Base Setup With Tailwind </div>
     </div>
   );
