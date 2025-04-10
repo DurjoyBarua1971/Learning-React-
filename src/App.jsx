ed3822c956042c7bc82f0879575d1356804b6e93
Durjoy Barua <durjoy.barua@brainstation-23.com>
2025-04-10 14:15:09 +0600

    Remove ClickCounter, HoverCounter, and WithCounter components; simplify App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import React from "react";
 import "./App.css";
-import ClickCounter from "./components/ClickCounter";
-import HoverCounter from "./components/HoverCounter";
 
 function App() {
   return (
-    <div className="font-roboto container mx-auto flex flex-col items-center justify-center h-screen bg-amber-300 text-gray-800 gap-8">
-      <ClickCounter />
-      <HoverCounter />
+    <div className="font-roboto container mx-auto flex flex-col items-center justify-center h-screen bg-amber-300 text-gray-800">
+      <div>Base Setup With Tailwind </div>
     </div>
   );
 }
