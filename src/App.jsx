b1602d0e57dd57ca65edd0cdaf322f5f0307b581
Durjoy Barua <durjoy.barua@brainstation-23.com>
2025-04-15 13:14:19 +0600

    Remove Counter and TodoList components along with their reducers; simplify App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import "./App.css";
-import { Counter } from "./components/Counter";
-import { TodoList } from "./components/TodoList";
 
 function App() {
   return (
     <div className="font-roboto container mx-auto flex flex-col items-center justify-center h-screen bg-amber-300 text-gray-800 gap-4">
-      <Counter />
-      <TodoList />
+      <h1 className="text-4xl font-bold">React + Vite + Tailwind CSS</h1>
     </div>
   );
 }
